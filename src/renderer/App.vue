d9709f9e5d36941700eda43cd4b20159a199647b
boletebrige <josip.ravas.broj@gmail.com>
2017-09-20 19:58:41 +0200

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <v-app id="example-1" toolbar footer dark>
+      <v-navigation-drawer
+        persistent
+        v-model="drawer"
+        light
+        enable-resize-watcher
+        absolute
+        dark
+      >
+        <v-list dense>
+          <v-list-tile>
+            <v-list-tile-action>
+              <router-link to="/"><v-icon>home</v-icon></router-link>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+              <v-list-tile-title><router-link to="system-information">System Information</router-link></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar dark fixed>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>{{ this.$route.name }}</v-toolbar-title>
+      </v-toolbar>
+      <main>
+        <v-container fluid>
+          <div>
+            <router-view></router-view>
+          </div>
+        </v-container>
+      </main>
+      <v-footer dark>
+        <span class="white--text">© 2017</span>
+      </v-footer>
+    </v-app>
+  </div>
+</template>
+
+
+
+<script>
+
+  export default {
+    name: 'clipboard',
+    data() {
+      return {
+        drawer: true,
+        card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
+      }
+    },
+  }
+</script>
+
+<style lang="scss"s>
+  @import '../../node_modules/vuetify/dist/vuetify.min.css' 
+</style>
---
+++ src/renderer/components/LandingPage/SystemInformation.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container>
+    <v-data-table
+        v-bind:headers="headers"
+        :items="items"
+        class="elevation-1"
+        hide-actions
+        dark
+      >
+      <template slot="items" scope="props">
+        <td>{{ props.item.name }}</td>
+        <td class="text-xs-right">{{ props.item.data }}</td>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+         headers: [
+          {
+            text: 'System Information',
+            align: 'left',
+            sortable: false,
+            value: 'name'
+          }
+        ],
+        items: [
+          {
+            value: false,
+            name: 'Electron:',
+            data: process.versions['atom-shell']
+          },
+          {
+            value: false,
+            name: 'Node:',
+            data: process.versions.node
+          },
+          {
+            value: false,
+            name: 'Vue.js',
+            data: require('vue/package.json').version
+          },
+          {
+            value: false,
+            name: 'Platform',
+            data: require('os').platform()
+          },
+          {
+            value: false,
+            name: 'Architecture',
+            data: require('os').arch()
+          },
+          {
+            value: false,
+            name: 'Number of CPU',
+            data: require('os').cpus().length
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
